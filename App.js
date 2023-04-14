4e1b434f2f7b9de905b3ecc76c3ccfe3ae7d2260
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-04-14 21:28:28 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,25 @@
+// const heading = React.createElement(
+  //   "h1",
+  //   { id: "heading", xyz: "abc" },
+  //   "Hello World From React!"
+// );
+
+//Nested Elements 
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div",{ id: "child" },
+    //Making Siblings
+    [
+      React.createElement("h1", {}, "I am an H1 Tag"),
+      React.createElement("h2", {}, "I am an H2 Tag"),
+    ]),
+    React.createElement("div",{ id: "child2" },[
+      //Making Siblings
+      [
+        React.createElement("h1", {}, "I am an H1 Tag"),
+        React.createElement("h2", {}, "I am an H2 Tag"),
+      ],
+    ]),
+]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
