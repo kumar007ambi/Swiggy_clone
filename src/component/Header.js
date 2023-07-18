48b0650bf479ddff7ae4a64803bc490f81cab20d
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-07-18 21:44:53 +0530

    added children routes and Link route to all pages

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 //Header
 const Header = () => {
   const [btnReactName, setBtnReactName] = useState("Login");
@@ -10,9 +11,9 @@ const Header = () => {
       </div>
       <div className="nav-item">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Conatct Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Conatct Us</Link></li>
           <li>Cart</li>
           <button
             className="login"
