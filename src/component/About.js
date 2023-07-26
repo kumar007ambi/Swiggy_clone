72b4abf4979ee21786ab81d23779cb8db9efa383
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-07-26 21:46:20 +0530

    added class based component

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,14 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <div><User/></div>
+      <div><UserClass/></div>
     </div>
   );
 };
