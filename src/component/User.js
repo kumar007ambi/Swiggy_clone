9c0bcfd4be2c1a49a306746f04ab3e97adcb27a1
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-07-27 21:50:41 +0530

    added class state and use of porps

--- src/component/User.js
+++ src/component/User.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
-const User = () => {
+const User = ({ name }) => {
+  const [ count ] = useState(0);
   return (
     <div className="user-card">
-      <h2>Name : Ambika Kumar Kewat</h2>
+      <h3>Count : {count}</h3>
+      <h2>Name : {name}</h2>
       <h3>Location : Kolkata</h3>
       <h4>Contact : ambikakewat007@gmail.com</h4>
     </div>
