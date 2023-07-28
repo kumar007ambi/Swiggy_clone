b52fb01474dc0582e60ad3057640d1851df40db1
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-07-28 21:40:30 +0530

    added setState which updates the state

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -5,14 +5,28 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 2,
     };
   }
 
   render() {
+    const { name } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
-        <h3>Count : {this.state.count}</h3>
-        <h2>Name : {this.props.name}</h2>
+        <h3>Count : {count}</h3>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Click me
+        </button>
+        {/* <h3>Count2 : {count2}</h3> */}
+
+        <h2>Name : {name}</h2>
         <h3>Location : Kolkata</h3>
         <h4>Contact : ambikakewat007@gmail.com</h4>
       </div>
