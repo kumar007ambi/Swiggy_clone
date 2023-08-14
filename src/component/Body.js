4f140e8a5a34ad69b2f71d33950d889f7be56424
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-08-14 22:12:22 +0530

    api change

--- src/component/Body.js
+++ src/component/Body.js
@@ -16,8 +16,9 @@ const Body = () => {
     const data = await fetch(WEB_API);
     const json = await data.json();
     //optional chaining
-    setListOfRest(json?.data?.cards[2]?.data?.data?.cards);
-    setFilteredRestuarnt(json?.data?.cards[2]?.data?.data?.cards)
+    setListOfRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log( setListOfRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants))
+    setFilteredRestuarnt(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   };
   //conditional rendering
   return listOfRest.length === 0 ? (
