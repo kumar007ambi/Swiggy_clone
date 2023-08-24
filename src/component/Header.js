8ee54329ea4a53c8495cdfd045b98283ea9e7ea8
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-08-24 21:26:55 +0530

    added tailwinds css classname

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
   const onlineStatus=useOnlineStatus();
 
   return (
-    <div className="header">
+    <div className="flex">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-56" src={LOGO_URL} />
       </div>
       <div className="nav-item">
         <ul>
