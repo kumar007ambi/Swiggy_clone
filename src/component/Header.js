319d83a6eb55aa0b1653924a872620381eeb717f
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-08-26 21:40:11 +0530

    added more css classes in our app

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,18 +9,18 @@ const Header = () => {
   const onlineStatus=useOnlineStatus();
 
   return (
-    <div className="flex">
-      <div className="container">
+    <div className="flex justify-between bg-slate-400">
+      <div className="logo-container">
         <img className="w-56" src={LOGO_URL} />
       </div>
-      <div className="nav-item">
-        <ul>
-          <li>Online Status : {onlineStatus===true?"🟢":"🔴"}</li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Conatct Us</Link></li>
-          <li><Link to="/grocery">Grocery</Link></li>
-          <li>Cart</li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status : {onlineStatus===true?"🟢":"🔴"}</li>
+          <li className="px-4"><Link to="/">Home</Link></li>
+          <li className="px-4"><Link to="/about">About Us</Link></li>
+          <li className="px-4"><Link to="/contact">Conatct Us</Link></li>
+          <li className="px-4"><Link to="/grocery">Grocery</Link></li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
