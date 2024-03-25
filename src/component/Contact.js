4a83b222cdbe64469369e5c5ecf76e9da1d5cf94
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-03-25 21:17:14 +0530

    started testing

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,7 +3,22 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-        <h1>I am Contact Page</h1>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+      <form>
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="message"
+        />
+        <button className=" border border-black p-2 m-2 bg-gray-100 rounded-lg">
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
