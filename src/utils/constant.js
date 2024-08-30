7e03b825e7286294d79d1e762e7866035d14d98b
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-08-30 21:55:22 +0530

    secondheader

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
 //Named Export
 export const LOGO_URL="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const WEB_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB";
+export const WEB_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const SWIGGY_MENU_URL =
 "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.6808046&lng=88.3757783&restaurantId=";
 export const GITHUBAPI="https://api.github.com/users/kumar007ambi"
