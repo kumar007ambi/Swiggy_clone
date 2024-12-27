31774308fd363d198541d7206587575351914616
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-12-27 21:38:28 +0530

    some changes

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -20,7 +20,7 @@ const ItemList = ({ items ,dummy }) => {
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+          className="p-3 m-3 border-gray-200 border-b-2 text-left flex justify-between"
         >
           <div className="w-9/12">
             <div className="py-2">
