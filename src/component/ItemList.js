7fb0b5fd63f4eb9e184b77b98e424c76411e9cf0
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2025-01-03 21:49:43 +0530

    some changes

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -37,7 +37,7 @@ const ItemList = ({ items, restaurant }) => {
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+          className="p-3 m-3 border-gray-200 border-b-2 text-left flex justify-between"
         >
           <div className="w-9/12">
             <div className="py-2">
