81d5b832ba05984c890a481f118c59222957c69d
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2025-01-13 21:34:36 +0530

    some changes

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
+    return (
+        <div>
             <div className="flex flex-row justify-center min-h-[104px] max-h-[136px] items-center bg-[#f0f0f5]">
                 <div className="w-[458px] max-h-[64px] my-auto mr-24 ">
                     <div className="text-[26px] leading-8 text-[#02060cbf] font-extrabold">
@@ -10,7 +10,7 @@ const Footer = () => {
                         now
                     </div>
                 </div>
-                <div className="flex flex-row justify-center h-16 my-auto mx-2">
+                <div className="flex flex-row justify-center h-16 my-auto mx-3">
                     <img
                         className="h-[64px] my-auto mx-[12px] "
                         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png"
@@ -27,12 +27,12 @@ const Footer = () => {
                 <div className="text-white flex items-center">
                     <span className="ml-3 text-lg font-semibold">Swiggy Clone @2025</span>
                 </div>
-                <div className="text-white">Made By 
+                <div className="text-white">Made By
                     <a href="https://www.linkedin.com/in/connect-with-ambika/" className="text-[#fc8019]" target="_blank"> Ambika Kumar Kewat</a>
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default Footer
