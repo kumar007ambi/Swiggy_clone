823883f4133bad0511cec35f3430eff5ee97f195
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2025-01-15 21:44:16 +0530

    some changes

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -11,7 +11,7 @@ const Cart = () => {
     const totalAmount = useSelector((store) => store.cart.totalAmount);
     const dispatch = useDispatch();
 
-    console.log("imageId", cartItems);
+    // console.log("imageId", cartItems);
 
 
     const navigate = useNavigate();
