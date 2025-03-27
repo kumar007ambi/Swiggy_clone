5e5bdab8493e7afcf2e34a9ecf1f47d381d4ef88
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2025-03-27 21:19:42 +0530

    some changess

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -62,8 +62,8 @@ const ItemList = ({ data, restaurant }) => {
 };
 export default ItemList;
               
-                {/* {item.card.info.price
-                  ? item.card.info.price / 100
+             
+                 {/*   ? item.card.info.price / 100
                   : item.card.info.defaultPrice / 100}
               </span>
             </div>
